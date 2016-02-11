97a4d2d913fe6b66d289a1e9503e1d6015efbd3d
Bill Harding <bill@tahzoo.com>
2016-02-11 12:14:43 -0800

    Improved QA and

--- Predictive_Modeling.py
+++ Predictive_Modeling.py
@@ -2,14 +2,17 @@
 import pandas as pd
 import numpy as np
 from sklearn.lda import LDA
-import datetime
+import sklearn as sk
+from datetime import datetime
+from collections import Counter
 
 #Step one getting the data:
 #specify my working directory:
 path = "C:\localmachine\Bill\{}"
 #I put it in this way so that I could just add CSV files as they come in. 
 myfiles = ["transactions2014.csv",
-			"transactions2015.csv"]
+			"transactions2015_1.csv",
+			"transactions2015_2.csv"]
 frames = [pd.read_csv(path.format(x)) for x in myfiles]
 df = pd.concat(frames)
 
@@ -21,17 +24,23 @@ df['filters'] = df['Category'].apply(lambda x: x in keep_cats)
 df = df[df['filters']]
 df = df.dropna().reset_index(drop=True)
 
-#adding a day of the week:
-def get_day_of_week(x):
+#pesky date_time issues:
+#fix that pesky date issue that I didn't fix before:
+df['isDate'] = df.Date.apply(lambda x: '/' in str(x))
+df.loc[df['isDate']==False,'Date'] = df.loc[df['isDate']==False,'Date of pull']
+df.head()
+
+def fixYear(x):
 	try:
-		mydate = datetime.datetime.strptime(x, '%m/%d/%Y')
+		y = datetime.strptime(x, '%m/%d/%Y')
 	except:
-		#I was inconsistent with my datestrings
-		#Why didn't I just use ISO format!
-		mydate = datetime.datetime.strptime(x, '%m/%d/%y')
-	return mydate.strftime('%A')
-	
-df['dayOfWeek'] = df['Date of pull'].apply(lambda x: get_day_of_week(x))
+		y = datetime.strptime(x, '%m/%d/%y')
+	return y
+df['dataObje'] = df['Date'].apply(lambda X: fixYear(X))
+
+
+#adding a day of the week:
+df['dayOfWeek'] = df['dataObje'].apply(lambda x: x.strftime('%A'))
 
 #then I'm using the "distance from saturday as a proxy for catagorical value"
 def dist_from_sat(x):
@@ -45,19 +54,31 @@ def dist_from_sat(x):
 	return myvalues[x]
 df['distFromSat'] = df['dayOfWeek'].apply(lambda x: dist_from_sat(x))
 	
-	
 #transform my data for the model:
 X = df.loc[:,['distFromSat','Hour','Amount']].values
 y = df.loc[:,'Category'].values
 
 #now I can run my model:
+X_train, X_test, y_train, y_test = sk.cross_validation.train_test_split(X, y, test_size=0.3, random_state=42)
+
 clf = LDA()
-clf.fit(X, y)
+clf.fit(X_train, y_train)
 
 #now see if my model has any validity:
+Y_results = clf.predict(X_test)
+#I want to see how the model is able to gues against data that the model has not yet seen:
+correct_guesses = Y_results[Y_results == y_test]
+correct_guesses_score = len(correct_guesses)/float(len(Y_results))
+
+#to score against: Here is the results of just guessing 'Out for drinks' every time.
+drinks_guesses = guess_drinks[np.repeat('Out for drinks',len(y_test)) == y_test]
+drinks_guesses_score = len(correct_guesses)/float(len(Y_results))
+
+drinks_guesses_score-float(correct_guesses_score)
+
+#apply that model back into the dataset:
 df['predictions'] = clf.predict(X)
 accuracy_of_model = len(df[df['predictions'] == df['Category']])/(len(df)*1.)
 accuracy_of_random_guess = 1./len(np.unique(y))
 
 print(accuracy_of_model-accuracy_of_random_guess)
-#0.212 over random isn't so bad :)
